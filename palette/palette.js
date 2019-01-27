093c076e789d2a880f384c38f881230feab65e33
Yanderella! <yanderelladev@len.meek.moe>
2019-01-27 16:10:51 +0100

    Create palette.js

---
+++ palette/palette.js
@@ -0,0 +1,18 @@
+// Snippets be like
+
+const palette = {
+  primary: {
+    a: '#AD8AE0',
+    b: '#EFE6FD',
+    c: '#CEB7F1',
+    d: '#8C63C9',
+    e: '#6D41AC',
+  },
+  complement: {
+    a: '#FFF493',
+    b: '#FFFCE6',
+    c: '#FFF8BB',
+    d: '#FFF171',
+    e: '#FDEC51',
+  }
+}
